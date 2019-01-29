5518822dd8bcf8edba5b72c9117beecabf4ece6c
Cristian <cristian.lfs@gmail.com>
2019-01-28 21:01:33 -0500

    add translate, and icons

--- Weather/views.py
+++ Weather/views.py
@@ -1,11 +1,16 @@
-#Django
+# Django
 from django.shortcuts import render, redirect
+from Weather.settings.base import BASE_DIR
 
-#Python
+# Python
 from urllib.request import urlopen
 import json
 import pytemperature
-from translate import Translator
+import os
+import goslate
+
+gs = goslate.Goslate()
+
 
 def start_view(request):
     """Start View."""
@@ -13,33 +18,34 @@ def start_view(request):
         lat = request.POST['lat']
         lon = request.POST['lon']
         if lat == '0' or lon == '0':
-             return render(request,'home.html',{'error':'Seleccione una cuidad'})
-        return redirect('weather', coordinates = lat+','+lon)
+            return render(request, 'home.html', {'error': 'Seleccione una cuidad'})
+        return redirect('weather', coordinates=lat + ',' + lon)
     return render(request, 'home.html')
 
-def weather(request,coordinates):
-    translator = Translator(to_lang="Spanish")
-    x=coordinates.split(',')
-    url = 'http://api.openweathermap.org/data/2.5/weather?lat='+x[0]+'&lon='+x[1]+'&APPID=6697b63dea7aa12280afbb7087640af5'
+
+def weather(request, coordinates):
+    x = coordinates.split(',')
+    url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + x[0] + '&lon=' + x[
+        1] + '&APPID=6697b63dea7aa12280afbb7087640af5'
     response = urlopen(url)
     data = json.load(response)
+    code = data['weather'][0]['id']
     main = data['weather'][0]['main']
-    main_es = translator.translate(main)
-    description = data['weather'][0]['description']
-    description_es = translator.translate(description)
-    description_es = description_es.replace('weather condition','')
-    icon = 'http://openweathermap.org/img/w/'+data['weather'][0]['icon']+'.png'
     temp = data['main']['temp']
     temp2c = pytemperature.k2c(temp)
     country = data['sys']['country']
     city_name = data['name']
     template = 'weather.html'
+    icons = json.loads(open(os.path.join(BASE_DIR, '../static/icons.json')).read())
+    icon = icons[str(code)]['icon']
+    description = icons[str(code)]['description_es']
+    main_es = gs.translate(str(main), 'es')
     context = {
         'main': main_es,
-        'description':description_es,
-        'icon':icon,
-        'temp':int(temp2c),
-        'country':country,
-        'city_name':city_name,
+        'temp': int(temp2c),
+        'country': country,
+        'city_name': city_name,
+        'icon': icon,
+        'description': description
     }
-    return render(request,template,context)
+    return render(request, template, context)
